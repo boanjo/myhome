83c8ba754d24645d3212894619961078cb023963
Anders Johansson <epkboan@boan.asus.laptop>
2019-07-12 15:39:08 +0200

    Added presence check for Robot Mower

---
+++ robo_mower/robo_mower.ino
@@ -0,0 +1,64 @@
+/*
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * http://www.mysensors.org/build/relay
+ */
+
+#define MY_DEBUG
+#define MY_RADIO_RFM69
+#define MY_IS_RFM69HW
+#define MY_RFM69_FREQUENCY RFM69_433MHZ // RFM69_433MHZ for development branch, RF69_433MHZ for master
+#define MY_RFM69_NEW_DRIVER 
+
+#define MY_REPEATER_FEATURE
+#define MY_NODE_ID 61
+
+#define MY_RFM69_NETWORKID (170)
+
+#include <MySensors.h>
+#include <Bounce2.h>
+
+#define CHILD_ID 3
+#define BUTTON_PIN  3  // Arduino Digital I/O pin for button/reed switch
+
+Bounce debouncer = Bounce(); 
+int oldValue=-1;
+
+// Change to V_LIGHT if you use S_LIGHT in presentation below
+MyMessage msg(CHILD_ID,V_TRIPPED);
+
+void setup()  
+{  
+  // Setup the button
+  pinMode(BUTTON_PIN,INPUT);
+  // Activate internal pull-up
+  digitalWrite(BUTTON_PIN,HIGH);
+
+  // After setting up the button, setup debouncer
+  debouncer.attach(BUTTON_PIN);
+  debouncer.interval(5);
+
+}
+
+void presentation() {
+  // Register binary input sensor to gw (they will be created as child devices)
+  // You can use S_DOOR, S_MOTION or S_LIGHT here depending on your usage. 
+  // If S_LIGHT is used, remember to update variable type you send in. See "msg" above.
+  present(CHILD_ID, S_DOOR);  
+}
+
+
+//  Check if digital input has changed and send in new value
+void loop() 
+{
+  debouncer.update();
+  // Get the update value
+  int value = debouncer.read();
+
+  if (value != oldValue) {
+     // Send in the new value
+     send(msg.set(value==HIGH ? 1 : 0));
+     oldValue = value;
+  }
+}
